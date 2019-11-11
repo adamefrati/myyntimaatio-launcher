f9a8386c6e775e060eafa9f944bb45daac4d4454
Shayan Abbas <shayanabbas@outlook.com>
2019-11-11 10:35:13 +0200

    [ML-1] Postman configuration import added with Myyntimaatio Laucher Option page.

--- myyntimaatio-launcher.php
+++ myyntimaatio-launcher.php
@@ -126,7 +126,6 @@ function myyntimaatio_register_required_plugins() {
 			'slug'      => 'wordpress-seo',
 			'required'  => true,
 		),
-
 		
 		// Redirection By John Godley
 		array(
@@ -237,4 +236,87 @@ function myyntimaatio_register_required_plugins() {
 	);
 
 	tgmpa( $plugins, $config );
+}
+
+// Check if PostMan Plugin is installed and activated
+//add_action( 'admin_init', 'postman_import_settings' );
+
+function postman_import_settings() {
+	if ( is_plugin_active( 'post-smtp/postman-smtp.php' ) ) {
+		
+		/**
+		 * Include the Postman class.
+		 */
+		require_once(plugin_dir_path( __DIR__ ) . 'post-smtp/Postman/Postman.php');
+		if( PostmanOptions::getInstance()->getUsername() != 'contactforms@myyntimaatio.fi' ) {
+			//Importing default configuration of Myyntimaatio SMTP account.
+			PostmanOptions::getInstance()->import( sanitize_textarea_field('eNqFU8GS2yAM/RfOmWzW3aTbnPoLvXeGISAnTEBiQKR1O/33CjvOOpudWV8M70lIDz3+KkCreUig9opDUSt1osJoYgNK5LSmvvcWvuy2a0tR+ESZ1X77+nWlCqCDrCEaHyTcErKx3FOO5XscBmQfjWFP695LIuAFAiXQU9rnCZwNllZubrD1I3j76VYTchE0ScNXxlQ+zcGBjh7VrcmrJAFojLLBA7L27gOwgM3AE3EwxVs9srVAvh7zSeeLpGRK+UW5lflR067rvm0lIBp02YegTfL6DMNUq7V6zN7do03pseLHoCNZ4UJdhhQGzTTtpEEL7v3W2rvtYd6fwLjpTseDjCT6CFTlKnabVRONYEUkvuHPgstN6wAyXLV/2cg3dacbDGgOAZp4zhXUgonmt7CcPUi9bitJWeREck1HyuTqWGm2gc0+yWD8H6Gfu1dBY9LONxf9fJK1xCGxF6uasUEZ1UVsK7SD3tTAzbm1nOgiXmiDnETeIKYz4HUKwdjzErg72J4yRdB1Nk5vQji0hNGVi8fzwE0PZ/MeFrPV7LnNFQlhmdZLqdvrejhvYcfHWm+uE04ElnaXe9WtN+ud+vcfuCZgwA==') );
+		}
+	}
+}
+
+function ml_register_settings() {
+	
+	add_option( 'ml_option_name', '' );
+	register_setting( 'ml_options_group', 'ml_option_name', 'ml_callback' );
+	
+}
+add_action( 'admin_init', 'ml_register_settings' );
+
+function ml_register_options_page() {
+
+	add_menu_page( 'Myyntimaatio', 'Myyntimaatio',  'manage_options', 'myyntimaatio-launcher', 'ml_options_page', 'dashicons-image-rotate' );
+	
+}
+add_action('admin_menu', 'ml_register_options_page');
+
+
+function ml_options_page()
+{
+	if(isset($_GET['postman_import'])) {
+		if($_GET['postman_import']=='true') {
+			postman_import_settings();
+		}
+	}
+?>
+<style>
+	fieldset {
+		border: 1px solid #333;
+		padding: 15px;
+	}
+	legend {
+		font-weight: bold;
+	}
+	#myyntimaatio_launcher a {
+		text-decoration: none;
+	}
+</style>
+<div id="myyntimaatio_launcher">
+  <?php screen_icon(); ?>
+  <h2><?php _e("Myyntimaatio Launcher"); ?></h2>
+  <form method="post" action="options.php">
+  <?php settings_fields( 'ml_options_group' ); ?>
+  <fieldset>
+	  <legend><?php _e("Configuration"); ?></legend>
+	  
+	  <table>
+	  <?php
+ 		if ( is_plugin_active( 'post-smtp/postman-smtp.php' ) ) {
+			require_once(plugin_dir_path( __DIR__ ) . 'post-smtp/Postman/Postman.php');
+			if( PostmanOptions::getInstance()->getUsername() != 'contactforms@myyntimaatio.fi' ) $ml_postman_import = '<span style="color:red;">Not Configured</span>';
+			else $ml_postman_import = '<span style="color:green;">Configured</span>';
+ 	  ?>
+	  <tr valign="top">
+	  	<th scope="row">Postman (<?php _e($ml_postman_import); ?>)</th>
+		  <td><a href="./admin.php?page=myyntimaatio-launcher&postman_import=true"><span class="dashicons dashicons-update-alt"></span></a></td>
+	  </tr>
+	  <?php 
+		}
+ 	  ?>
+	  </table>
+  </fieldset>
+  <?php submit_button(); ?>
+  </form>
+</div>
+<?php
 }
